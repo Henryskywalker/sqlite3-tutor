d04b23441eb7c61a8320f1e2fd8f963347633ceb
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2018-01-02 23:28:01 -0300

    Add files via upload

--- tutorial_02.py
+++ tutorial_02.py
@@ -5,10 +5,21 @@ import sqlite3
 
 class SegundoTutorial:
 	def __init__(self):
-		self.conexao = sqlite3.connect("banco")
+		self.conexao = sqlite3.connect("banco", timeout=1)
+		#self.cursor
 
 	def adiocionar_dados(self, nome, sobrenome, endereco):
 		self.conexao.execute("INSERT INTO usuarios VALUES (NULL, '{nome}', '{sobrenome}','{endereco}')".format(nome=nome, sobrenome=sobrenome, endereco=endereco))
 		self.conexao.commit()
 
-SegundoTutorial().adiocionar_dados('Henrique', 'Nascimento', 'Rua do Comercio')
+	def consultar_tabela(self):
+		self.verifica = self.conexao.execute("SELECT nome FROM usuarios")
+		for i in self.verifica:
+			print(i)
+
+	def alteracao_nome(self, nome, id):
+		self.conexao.execute("UPDATE `usuarios` SET `nome`='{nome}' WHERE `id`='{id}'".format(nome=nome, id=id))
+		self.conexao.commit()
+
+SegundoTutorial().alteracao_nome("Carlos", "1")
+#SegundoTutorial().adiocionar_dados('Fernando','Porto','25')
